31b380b346fdf3747c0ac8b3295f4c98be4d613a
Andrey <andrey@zavrichko.dev>
2024-08-09 20:20:24 +0300

    hw15

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,41 +2,46 @@ import pytest
 from selene import browser
 
 
-@pytest.fixture(scope='function', params=[(1920, 1080), (1280, 720), (430, 932)])
-def setup_browser(request):
-    width, height = request.param
-
-    browser.config.window_width = width
-    browser.config.window_height = height
+@pytest.fixture(scope='function', params=[(1280, 720), (1920, 1080), (2560, 1440)],
+                ids=['WEB_1280', 'WEB_1920', 'WEB_2560'])
+def browser_management_desktop(request):
     browser.config.base_url = 'https://github.com'
 
-    if width > 800:
+    width, height = request.param
+    browser.config.window_height = height
+    browser.config.window_width = width
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope='function', params=[(414, 896), (360, 740), (480, 854)],
+                ids=['Mobile_414', 'Mobile_360', 'Mobile_480'])
+def browser_management_mobile(request):
+    browser.config.base_url = 'https://github.com'
+
+    width, height = request.param
+    browser.config.window_height = height
+    browser.config.window_width = width
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope='function', params=[(1280, 720), (1920, 1080), (414, 896)],
+                ids=['WEB_1280', 'WEB_1920', 'Mobile_414'])
+def browser_management(request):
+    browser.config.base_url = 'https://github.com'
+
+    width, height = request.param
+    browser.config.window_height = height
+    browser.config.window_width = width
+
+    if width >= 800:
         yield 'desktop'
     else:
         yield 'mobile'
-
-    browser.quit()
-
-
-@pytest.fixture(scope='function', params=[(430, 932), (390, 844)])
-def setup_browser_mobile(request):
-    width, height = request.param
-    browser.config.window_width = width
-    browser.config.window_height = height
-    browser.config.base_url = 'https://github.com'
-
-    yield
-
-    browser.quit()
-
-
-@pytest.fixture(scope='function', params=[(1920, 1080), (1280, 720)])
-def setup_browser_desktop(request):
-    width, height = request.param
-    browser.config.window_width = width
-    browser.config.window_height = height
-    browser.config.base_url = 'https://github.com'
-
-    yield
 
     browser.quit()
